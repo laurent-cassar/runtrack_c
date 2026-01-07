7c6048e9cfeea81658c5f4bfcff5f1263577a144
unknown <you@exam.io>
2026-01-07 20:39:29 +0100

    Demander à l'utilisateur d'entrer une chaîne et l'afficher

--- Jour01/Job01/main.c
+++ Jour01/Job01/main.c
@@ -2,14 +2,31 @@
 
 int main(void)
 {
+    char buffer[1024];
+    int bytes_read;
+    
+    // Test initial
     my_putchar('A');
     my_putchar('\n');
     
     my_putstr("Hello, World!");
     my_putchar('\n');
     
-    my_putstr("Test de la fonction my_putstr");
-    my_putchar('\n');
+    // Demander à l'utilisateur d'entrer une chaîne
+    my_putstr("Entrez une chaine de caracteres: ");
+    
+    // Lire l'entrée utilisateur
+    bytes_read = read(0, buffer, sizeof(buffer) - 1);
+    
+    if (bytes_read > 0)
+    {
+        // Ajouter le caractère de fin de chaîne
+        buffer[bytes_read] = '\0';
+        
+        // Afficher ce que l'utilisateur a entré
+        my_putstr("Vous avez entre: ");
+        my_putstr(buffer);
+    }
     
     return 0;
 }
