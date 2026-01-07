601a2f84dd4dea1b91215fd44180ce05152a290a
unknown <you@exam.io>
2026-01-07 20:32:55 +0100

    Update optimisation de code

--- Jour01/Job01/my_putstr.c
+++ Jour01/Job01/my_putstr.c
@@ -2,12 +2,10 @@
 
 void my_putstr(char *str)
 {
-    int i;
-
-    i = 0;
+    int i = 0;
+    
     while (str[i] != '\0')
     {
-        my_putchar(str[i]);
-        i++;
+        my_putchar(str[i]); i++;
     }
 }
